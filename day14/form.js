b8ffdd5fd28b1ab1ac612ec4d382ffb6bf1f9f27
Ray Chi <s103122@mjjh.ntct.edu.tw>
2022-08-13 18:21:07 +0800

    completed types: text, textarea, checkbox, radio, select

--- day14/form.js
+++ day14/form.js
@@ -2,20 +2,29 @@ function Form() {
     const [formData, setformData] = React.useState(
         {
             firstName: "",
-            lastName: ""
+            lastName: "",
+            email: "",
+            content: "",
+            employment: "",
+            isFriendly: true,
+            facColor: ""
         }
     )
 
-    console.log(formData)
+    // console.log(formData.favColor)
+
     function handleChange(event) {
+        const { name, value, type, checked } = event.target
+
+        // 若非 "checkbox" 則 checked 變數視為 "undefined"
+        // console.log(type, checked)
+
         setformData(preFormData => {
             return {
                 ...preFormData,
-                // ES6 - "Computed Properties"
-                [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
-
     }
 
     return (
@@ -24,14 +33,80 @@ function Form() {
                 type="text"
                 placeholder="First Name"
                 onChange={handleChange}
-                name="firstName">
-            </input>
+                value={formData.firstName}
+                name="firstName" />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
-                name="LastName">
-            </input>
+                value={formData.lastName}
+                name="lastName" />
+            <input
+                type="email"
+                placeholder="email"
+                onChange={handleChange}
+                value={formData.email}
+                name="email" />
+            <textarea
+                value={formData.content}
+                name="content"
+                onChange={handleChange}
+                placeholder="Type some text."
+            />
+            <input
+                type="checkbox"
+                name="isFriendly"
+                onChange={handleChange}
+                id="isFriendly"
+                checked={formData.isFriendly}
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+
+            <fieldset>
+                <legend>Current employ status</legend>
+
+                <input
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployment"
+                    onChange={handleChange}
+                    checked={formData.employment === "unemployment"}
+                />
+                <label htmlFor="unemployment">Unemployment</label>
+                <input
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    onChange={handleChange}
+                    checked={formData.employment === "part-time"}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <input
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    onChange={handleChange}
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                />
+                <label htmlFor="full-time">Full-time</label>
+            </fieldset>
+
+            <select
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+            >
+                <option value="">-- Select a Color --</option>
+                <option value="red">Red</option>
+                <option value="blue">Blue</option>
+                <option value="green">Green</option>
+                <option value="yellow">Yellow</option>
+                <option value="violet">Violet</option>
+            </select>
         </form>
     )
 }
