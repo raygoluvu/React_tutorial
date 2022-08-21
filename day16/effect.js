fef391f7ac93a787b1c7854285509dc15a4154d5
Ray Chi <s103122@mjjh.ntct.edu.tw>
2022-08-21 14:33:42 +0800

    利用 fetch 方法從 api 獲取資料

--- day16/effect.js
+++ day16/effect.js
@@ -1,21 +1,43 @@
 
+function Count(props) {
+    return (
+        <div>
+            <div>The count is {props.count}</div>
+        </div>
+    )
+}
+
+function Api(props) {
+    return (
+        <div>
+            <pre>
+                {JSON.stringify(props.data, null, 2)}
+            </pre>
+        </div>
+    )
+}
+
 function App() {
 
-    const [count, setCount] = React.useState(0)
-
+    const [count, setCount] = React.useState(1)
+    const [starWarsData, setStarWarsData] = React.useState({})
     function handleClick() {
         setCount(pre => pre + 1)
     }
 
     React.useEffect(
         function () {
-            console.log("effect ran.")
+            fetch(`https://swapi.dev/api/people/${count}`)
+                .then(res => res.json())
+                .then(data => setStarWarsData(data))
         }, [count])
+
 
     return (
         <div>
-            <div>The count is {count}</div>
-            <button onClick={handleClick}>Add</button>
+            <Count count={count}></Count>
+            <button onClick={handleClick}>Get Next Charactor</button>
+            <Api data={starWarsData}></Api>
         </div>
     )
 }
